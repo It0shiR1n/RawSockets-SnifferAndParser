e6d29feee4900049db460265fd2b25624e5e7f7f
Itoshi Rin <86209431+It0shiR1n@users.noreply.github.com>
2025-01-12 22:06:43 +0000

    Update BaseSniffer.c

--- BaseSniffer.c
+++ BaseSniffer.c
@@ -1,17 +1,14 @@
-// REMEBER TO PUT YOUR NIC IN TO A PROMISCUOUS MODE
-// Exists a tool called cscope, learn how to use then.
-
 // Continue from this Vídeo: https://youtu.be/DhI62V2iOBM?list=PL9ueu6QbfMC3SYOBOkDHed09gnsGlhfVl
 
-#include <stdio.h>            // For printf and standard I/O functions
-#include <stdlib.h>           // For memory functions
-#include <string.h>           // For bzero and strncpy functions
-#include <unistd.h>           // For close function
-#include <sys/socket.h>       // For socket functions
-#include <linux/if_ether.h>   // For ETH_P_IP and Ethernet header definitions
-#include <linux/if_packet.h>  // For sockaddr_ll and packet interface definitions
-#include <net/if.h>           // For struct ifreq and interface names
-#include <sys/ioctl.h>        // For ioctl function
+#include <stdio.h>            
+#include <stdlib.h>           
+#include <string.h>           
+#include <unistd.h>           
+#include <sys/socket.h>       
+#include <linux/if_ether.h>   
+#include <linux/if_packet.h>  
+#include <net/if.h>            
+#include <sys/ioctl.h>        
 
 #include "ParsingPacketsETH.h"
 #include "ParsingPacketsIP.h"
@@ -20,15 +17,15 @@
 
 
 int bindRawSocketToIface(char *device, int sockfd, int protocol){
-    struct sockaddr_ll iface;          // Define uma estrutura para receber
-    struct ifreq ifaceRequisition;     // Define uma requisição de interface
+    struct sockaddr_ll iface;          
+    struct ifreq ifaceRequisition;     
 
-    bzero(&iface, sizeof(iface));                         //| - Poupalate the memory space with zeros (to avoid more problems with chars)
-    bzero(&ifaceRequisition, sizeof(ifaceRequisition));   //|
+    bzero(&iface, sizeof(iface));                         
+    bzero(&ifaceRequisition, sizeof(ifaceRequisition));   
 
-    strncpy((char *) ifaceRequisition.ifr_name, device, IFNAMSIZ); // movimenta o valor de Device para ifr_name de tamanho "IFNAMSIZ" (Adiciona a placa de rede)
+    strncpy((char *) ifaceRequisition.ifr_name, device, IFNAMSIZ); 
 
-    if((ioctl(sockfd, SIOCGIFINDEX, &ifaceRequisition)) == -1 ){    // Load the IP Protocol from the Ethernet Frame Header
+    if((ioctl(sockfd, SIOCGIFINDEX, &ifaceRequisition)) == -1 ){    
         printf("Error to get the interface index! \n");
         exit(-1);
         
@@ -41,18 +38,16 @@ int bindRawSocketToIface(char *device, int sockfd, int protocol){
 
 
 int main(){ 
-    int sockfd;                                             // Socket descriptor
-    unsigned char packet_buffer[1024];                      // Buffer to receive packet
+    int sockfd;                                             
+    unsigned char packet_buffer[1024];                      
 
     int confirming; 
     int packet_to_sniff;
 
-    struct sockaddr_ll packet_info;                         // Informations of the packets provided by the kernel
+    struct sockaddr_ll packet_info;                         
     unsigned int packet_info_size = sizeof(packet_info_size);
 
-    
-    // Creating the Raw Socket
-    sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_IP));  // Criando um socket de baixo nível que vão receber Frames Ethernet
+    sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_IP));  
 
     if(sockfd == -1){
         printf("Error in socket creation");
